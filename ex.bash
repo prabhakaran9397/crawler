34d045ee62c109ee9534c1bd9adab87e3fb6022a
Prabhakaran Senthilnathan <prabhakaran9397@users.noreply.github.com>
2016-07-23 23:40:50 +0530

    Update ex.bash

--- ex.bash
+++ ex.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
-sed -i "s/prabhakaran9397/$2/g" `pwd`/e0.bash
-sed -i "s/qwerty931997/$3/g" `pwd`/e0.bash
+if [ $1 = "-k" ]; then
+	sudo kill -9 `ps aux | grep "e[0-9]*.bash" | awk '{print$2}'`
+	exit 0
+fi
+if [ $# -ne 3]; then
+	echo "Please use like this => bash ex.bash 10 hello@gmail.com password"
+	exit 1
+fi
+sed -i "s/email/$2/g" `pwd`/e0.bash
+sed -i "s/dalda/$3/g" `pwd`/e0.bash
+sed -i "s/@/%40/g" `pwd` /e0.bash
 for i in `seq 1 $1`;
 do
 	file="e"$i."bash"
